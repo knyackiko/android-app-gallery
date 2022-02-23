fe28c1c6f520c6d703b1f74ea0ec22386ea67f94
knyackiko <33259005+knyackiko@users.noreply.github.com>
2022-02-23 21:32:17 +0900

    Add navigation

--- HelloComposeUI/app/src/main/java/com/github/knyackiko/hellocomposeui/MainActivity.kt
+++ HelloComposeUI/app/src/main/java/com/github/knyackiko/hellocomposeui/MainActivity.kt
@@ -3,13 +3,25 @@ package com.github.knyackiko.hellocomposeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.github.knyackiko.hellocomposeui.ui.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            App()
         }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "/") {
+        composable("/") { MainScreen() }
     }
 }
